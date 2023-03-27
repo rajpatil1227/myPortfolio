addb67bb0e8403fedae0963fbdd1bdab6ccc7419
rajpatil1227 <raj.patil20@pccoepune.org>
2023-03-27 20:56:03 +0530

    commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,66 @@
+import { useState, useEffect } from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import raj from '../assets/img/rajj.png';
+import navIcon1 from '../assets/img/linkedin.png';
+import navIcon2 from '../assets/img/tweet.png';
+import navIcon3 from '../assets/img/insta.png';
+import { HashLink } from 'react-router-hash-link';
+import {
+  BrowserRouter as Router
+} from "react-router-dom";
+
+export const NavBar = () => {
+
+  const [activeLink, setActiveLink] = useState('home');
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    }
+
+    window.addEventListener("scroll", onScroll);
+
+    return () => window.removeEventListener("scroll", onScroll);
+  }, [])
+
+  const onUpdateActiveLink = (value) => {
+    setActiveLink(value);
+  }
+
+  return (
+    <Router>
+      <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
+        <Container>
+          <Navbar.Brand href="/">
+            <img src={raj} alt="Logo" style={{borderRadius:'50%', width:'80px'}}/>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav">
+            <span className="navbar-toggler-icon"></span>
+          </Navbar.Toggle>
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
+              <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
+              <Nav.Link href="#project" className={activeLink === 'project' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
+            </Nav>
+            <span className="navbar-text">
+              <div className="social-icon">
+                <a href="https://www.linkedin.com/in/raj-patil-2870551b0/"><img src={navIcon1} alt="" /></a>
+                <a href="https://twitter.com/patilraj1227"><img src={navIcon2} alt="" /></a>
+                <a href="https://www.instagram.com/raj_patil_1227/"><img src={navIcon3} alt="" /></a>
+              </div>
+              <HashLink to='#connect'>
+                <button className="vvd"><span>Let’s Connect</span></button>
+              </HashLink>
+            </span>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </Router>
+  )
+}
---
+++ src/components/Projects.js
@@ -0,0 +1,144 @@
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+import projImg1 from "../assets/img/news.png";
+import projImg2 from "../assets/img/twit.png";
+import projImg3 from "../assets/img/imdb.png";
+import cart from "../assets/img/cart.png";
+import food from "../assets/img/food.png";
+import menu from "../assets/img/menu.png";
+import bbms from "../assets/img/bbms.png";
+import colorSharp2 from "../assets/img/color-sharp2.png";
+// import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
+export const Projects = () => {
+
+  const projects = [
+    {
+      title: "News App",
+      description: "A News App using ReactJs",
+      imgUrl: projImg1,
+      link : "/project",
+    },
+    {
+      title: "Tweet Quotes",
+      description: "On clicking on Twitter logo you can tweet Quotes",
+      imgUrl: projImg2,
+      link : "https://tweetquotes1227.netlify.app/",
+    },
+    {
+      title: "IMDB Rating",
+      description: "Gives IMDB Rating of Searched Movie",
+      imgUrl: projImg3,
+      link : "https://rjimdb.netlify.app/",
+    }
+  ];
+
+  const mern = [
+    {
+      title: "Food App",
+      description: "Home Page",
+      imgUrl: food,
+      link : "",
+    },
+    {
+      title: "Food App",
+      description: "menu",
+      imgUrl: menu,
+      link : "",
+    },
+    {
+      title: "Food App",
+      description: "cart",
+      imgUrl: cart,
+      link : "",
+    }
+  ];
+
+  const php = [
+    {
+      title: "Blood Bank Management",
+      description: "Using PHP",
+      imgUrl: bbms,
+      link : "",
+    }
+  ];
+
+  return (
+    <section className="project" id="project">
+      <Container>
+        <Row>
+          <Col size={12}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
+                <h2 style={{fontFamily:'serif'}}>Projects</h2><br />
+                <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                    <Nav.Item>
+                      <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                    </Nav.Item>
+                  </Nav>
+                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
+                    <Tab.Pane eventKey="first">
+                      <Row>
+                      <h2 style={{fontFamily:"cursive", color:'violet'}}>ReactJs</h2><br /><br /><br />
+                        {
+                          projects.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="second">
+                    <Row>
+                    <h2 style={{fontFamily:"cursive", color:'violet'}}>MERN Development</h2><br /><br /><br />
+                        {
+                          mern.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="third">
+                    <h2 style={{fontFamily:"cursive", color:'violet'}}>PHP Development</h2><br /><br /><br />
+                    <div className="d-flex justify-content-center">
+                        {
+                          php.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </div>
+                    </Tab.Pane>
+                  </Tab.Content>
+                </Tab.Container>
+              </div>}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+      <img className="background-image-right" src={colorSharp2}></img>
+    </section>
+  )
+}
